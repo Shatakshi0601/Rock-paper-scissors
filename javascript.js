89a616fe3f36ec8786840c6b1da52cbf8fdd05a9
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-09-20 17:32:41 +0530

    Update javascript.js with new content

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+function getComputerChoice() {
+   let randomNumber = Math.floor(Math.random() * 3);
+       if (randomNumber === 0) {
+           return("rock");
+     } else if (randomNumber === 1) {
+           return("paper"); 
+     } else {
+           return("scissors");
+
+     }   
+} 
+
+function getHumanChoice() {
+    let response =  prompt("Rock, paper or scissors?");
+    if (response === null) {
+        alert ("Bye! Close the window or click okay to continue the game.");
+    } else if (response.toLowerCase() === "rock") {
+        return ("rock");
+    } else if (response.toLowerCase() === "paper") {
+        return ("paper");
+    } else if (response.toLowerCase() === "scissors") {
+        return ("scissors");
+    } else {
+        alert ("Incorrect response, try again in the next round!");
+    }
+}
+

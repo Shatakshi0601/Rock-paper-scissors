c89d67cea2647ba95297a3cbcb9f40f043a42061
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-09-21 15:26:19 +0530

    Update javascript.js with new content

--- javascript.js
+++ javascript.js
@@ -33,7 +33,7 @@ function playRound(humanChoice, computerChoice) {
         humanChoice === "paper" && computerChoice ==="rock" || 
         humanChoice === "scissors" && computerChoice ==="paper") {
         humanScore++;
-        alert("You Won" , humanScore ,"|" ,computerScore )  
+        alert("You Won")  
     
     } else if (humanChoice === "rock" && computerChoice ==="paper" || 
         humanChoice === "paper" && computerChoice ==="scissors" || 

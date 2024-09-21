1180bc4cd2990e6d33298fa659dc8cc041857049
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-09-21 10:18:24 +0530

    Update javascript.js with new content

--- javascript.js
+++ javascript.js
@@ -7,21 +7,6 @@ function getComputerChoice() {
      } else {
            return("scissors");
 
-     }   
+     }  
+     console.log(randomNumber) 
 } 
-
-function getHumanChoice() {
-    let response =  prompt("Rock, paper or scissors?");
-    if (response === null) {
-        alert ("Bye! Close the window or click okay to continue the game.");
-    } else if (response.toLowerCase() === "rock") {
-        return ("rock");
-    } else if (response.toLowerCase() === "paper") {
-        return ("paper");
-    } else if (response.toLowerCase() === "scissors") {
-        return ("scissors");
-    } else {
-        alert ("Incorrect response, try again in the next round!");
-    }
-}
-
